c8bdfc07937b7a5d907a59777a396b074acea579
Stealthy20 <kjellin.fredrik@live.se>
2023-01-16 09:34:56 +0000

    Hooks dne

--- my-app/src/components/UseEffectCounter.js
+++ my-app/src/components/UseEffectCounter.js
@@ -2,18 +2,32 @@ import React, {useState, useEffect} from 'react'
 
 function UseEffectCounter() {
     const [count, setCount] = useState(0);
+    const [time, setTime] = useState(0);
+
     useEffect(() => {
+        console.log('count 1 effect');
         document.title = count;
-    });
-  return (
-    <div>
-        <button onClick={
-            () => setCount (count => count + 1)
-        }>
-            Increment Count ({count})
-        </button>
-    </div>
-  )
+    }, [count]);
+    useEffect(()=>{
+        console.log('Creating timer');
+        const interval = setInterval(() => {
+            setTime(time => time + 1);
+            console.log('counting');
+        }, 1000);
+        return () => {
+            clearInterval (interval)
+        }
+    }, []);
+    return (
+        <div>
+            <button onClick={
+                () => setCount(count => count + 1)
+            }>
+                Increment Count ({count})
+            </button>
+            <h2>Time is {time}</h2>
+        </div>
+    )
 }
 
 export default UseEffectCounter
